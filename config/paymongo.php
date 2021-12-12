c14391564c5020d24a698cfb2c790886f357ed0c
roidMB <roid.blancaflor@gmail.com>
2021-12-12 22:10:40 +0800

    update paymongo config

--- config/paymongo.php
+++ config/paymongo.php
@@ -1,7 +1,7 @@
 <?php 
 return [
     'base_api' => 'https://api.paymongo.com/v1',
-    'sucess_url' => 'http://localhost:8080/checkout/gcashsuccess',
+    'success_url' => 'http://localhost:8080/checkout/gcashsuccess',
     'fail_url' => 'http://localhost:8080/checkout/fail',
     'public_key' => 'pk_test_B7rW47bdoc1Nvd2MFiUDhfMC',
     'secret_key' => 'sk_test_m9f3aWRiVSbyRJgLUDwJKwmp',
