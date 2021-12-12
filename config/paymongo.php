046e6c4938f18f3942b645f097fd10cfd6384c36
roidMB <roid.blancaflor@gmail.com>
2021-12-12 21:54:22 +0800

    add config for paymongo

---
+++ config/paymongo.php
@@ -0,0 +1,9 @@
+<?php 
+return [
+    'base_api' => 'https://api.paymongo.com/v1',
+    'sucess_url' => 'http://localhost:8080/checkout/gcashsuccess',
+    'fail_url' => 'http://localhost:8080/checkout/fail',
+    'public_key' => 'pk_test_B7rW47bdoc1Nvd2MFiUDhfMC',
+    'secret_key' => 'sk_test_m9f3aWRiVSbyRJgLUDwJKwmp',
+    'basic_auth' => 'cGtfdGVzdF9CN3JXNDdiZG9jMU52ZDJNRmlVRGhmTUMK'
+];
